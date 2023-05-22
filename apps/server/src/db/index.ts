64a26cd43202c4fec1cfd523416aec165ff02161
Logan Martlew <logan.martlew@gmail.com>
2023-05-22 21:53:07 +1200

    Changed to postgres

--- apps/server/src/db/index.ts
+++ apps/server/src/db/index.ts
@@ -1,4 +1,16 @@
 import { PrismaClient } from '@prisma/client';
+import { PrismaClientOptions } from '@prisma/client/runtime';
+
+const options: PrismaClientOptions =
+  process.env.NODE_ENV === 'production'
+    ? {}
+    : {
+        datasources: {
+          db: {
+            url: 'postgres://root:password@localhost:2347/iotalarm?schema=public',
+          },
+        },
+      };
 
 const db = new PrismaClient();
 
